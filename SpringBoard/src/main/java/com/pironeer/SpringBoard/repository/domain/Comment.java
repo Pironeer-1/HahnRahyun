c89413b0688b67a8bc60e5bd9cd3a3035fe5b85e
Rahyuni <hanrahyun@naver.com>
2024-09-19 23:41:27 +0900

    Date annotation 추가

--- SpringBoard/src/main/java/com/pironeer/SpringBoard/repository/domain/Comment.java
+++ SpringBoard/src/main/java/com/pironeer/SpringBoard/repository/domain/Comment.java
@@ -1,9 +1,11 @@
 package com.pironeer.SpringBoard.repository.domain;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class Comment {
     private Long cmt_id; // 댓글 id (pk)
     private Long topic_id; // 게시물 id (외래키)
@@ -12,7 +14,12 @@ public class Comment {
     private LocalDateTime cmt_createdAt; // 댓글 생성 시간
 
     @Builder
-    public Comment(Long cmt_id, Long topic_id, Long parent_id, String cmt_cnt, LocalDateTime cmt_createdAt) {
+    public Comment(
+            Long cmt_id,
+            Long topic_id,
+            Long parent_id,
+            String cmt_cnt,
+            LocalDateTime cmt_createdAt) {
         this.cmt_id = cmt_id;
         this.topic_id = topic_id;
         this.parent_id = parent_id;
