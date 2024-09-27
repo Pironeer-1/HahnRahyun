250b45989428b3db273e6b8274fb630eb09b0762
Rahyuni <hanrahyun@naver.com>
2024-09-27 23:31:51 +0900

    � [fix] Repository annotation

--- templateCODE/src/main/java/com/pironeer/templateCODE/member/repository/MemberMemoryRepository.java
+++ templateCODE/src/main/java/com/pironeer/templateCODE/member/repository/MemberMemoryRepository.java
@@ -1,6 +1,7 @@
 package com.pironeer.templateCODE.member.repository;
 
 import com.pironeer.templateCODE.member.entity.Member;
+import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
 import java.util.HashMap;
@@ -9,6 +10,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class MemberMemoryRepository implements MemberRepository {
     private final AtomicLong memberIdxGenerator = new AtomicLong(0);
     private final Map<Long, Member> memberMap = new HashMap<>();
