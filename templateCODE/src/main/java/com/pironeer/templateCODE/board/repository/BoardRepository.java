32529db1d06b8ee9143b400d2e90d863395f44d6
Rahyuni <hanrahyun@naver.com>
2024-09-27 22:16:15 +0900

    ✨ [feat] Repository 추상화

--- templateCODE/src/main/java/com/pironeer/templateCODE/board/repository/BoardRepository.java
+++ templateCODE/src/main/java/com/pironeer/templateCODE/board/repository/BoardRepository.java
@@ -1,44 +1,18 @@
 package com.pironeer.templateCODE.board.repository;
 
 import com.pironeer.templateCODE.board.entity.Board;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.Assert;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
-@Repository
-public class BoardRepository {
-    private final AtomicLong boardIdxGenerator = new AtomicLong(0);
-    private final HashMap<Long, Board> boardMap = new HashMap<>();
+public interface BoardRepository {
+    void save(Board board);
 
-    // 게시글 저장
-    public void save(Board board) {
-        Long boardId = boardIdxGenerator.incrementAndGet();
-        board.setId(boardId);
-        boardMap.put(boardId, board);
-    }
+    List<Board> findAll();
 
-    // 게시글 전체 조회
-    public List<Board> findAll() {
-        return boardMap.values().stream().toList();
-    }
+    Optional<Board> findById(Long id);
 
-    // 게시글 id로 조회하기
-    public Optional<Board> findById(Long id) {
-        Assert.notNull(id, "ID는 null일 수 없습니다");
-        return Optional.ofNullable(boardMap.get(id));
-    }
+    void update(Board board);
 
-    // 게시글 수정
-    public void update(Board board) {
-        boardMap.replace(board.getId(), board);
-    }
-
-    // 게시글 삭제
-    public void deleteById(Long id) {
-        boardMap.remove(id);
-    }
+    void deleteById(Long id);
 }
--- templateCODE/src/main/java/com/pironeer/templateCODE/board/service/BoardService.java
+++ templateCODE/src/main/java/com/pironeer/templateCODE/board/service/BoardService.java
@@ -4,45 +4,44 @@ import com.pironeer.templateCODE.board.dto.request.BoardCreateRequest;
 import com.pironeer.templateCODE.board.dto.request.BoardUpdateRequest;
 import com.pironeer.templateCODE.board.dto.response.BoardResponse;
 import com.pironeer.templateCODE.board.entity.Board;
-import com.pironeer.templateCODE.board.repository.BoardRepository;
+import com.pironeer.templateCODE.board.repository.BoardMemoryRepository;
 import com.pironeer.templateCODE.global.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-    private final BoardRepository boardRepository;
+    private final BoardMemoryRepository boardMemoryRepository;
     public void save(BoardCreateRequest request) {
-        boardRepository.save(BoardMapper.from(request));
+        boardMemoryRepository.save(BoardMapper.from(request));
     }
 
     // 게시글 전체 조회하기
     public List<BoardResponse> findAll() {
-        List<Board> boards = boardRepository.findAll();
+        List<Board> boards = boardMemoryRepository.findAll();
         return boards.stream().map(BoardResponse::of).toList();
     }
 
     // 게시글 id로 조회하기
     public BoardResponse findById(Long id) {
-        Board board = boardRepository.findById(id)
+        Board board = boardMemoryRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("TOPIC NOT FOUND"));
         return BoardResponse.of(board);
     }
 
     // 게시글 수정
     public BoardResponse update(BoardUpdateRequest request) {
-        Board board = boardRepository.findById(request.id())
+        Board board = boardMemoryRepository.findById(request.id())
                 .orElseThrow(() -> new RuntimeException("TOPIC NOT FOUND"));
-        boardRepository.update(board.update(request));
+        boardMemoryRepository.update(board.update(request));
         return BoardResponse.of(board);
     }
 
     // 게시글 삭제
     public void deleteById(Long id) {
-        boardRepository.deleteById(id);
+        boardMemoryRepository.deleteById(id);
     }
 }
