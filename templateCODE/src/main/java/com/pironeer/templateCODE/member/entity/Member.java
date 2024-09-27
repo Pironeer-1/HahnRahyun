7802f3e24c76f32426e5bc34512a39d9f0d114f8
Rahyuni <hanrahyun@naver.com>
2024-09-27 22:42:34 +0900

    ✨ [feat] entity - Member

---
+++ templateCODE/src/main/java/com/pironeer/templateCODE/member/entity/Member.java
@@ -0,0 +1,22 @@
+package com.pironeer.templateCODE.member.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+    private String memberId;
+    private String password;
+    private String name;
+
+    @Builder
+    public Member(
+            String memberId,
+            String password,
+            String name) {
+        this.memberId = memberId;
+        this.password = password;
+        this.name = name;
+    }
+}
